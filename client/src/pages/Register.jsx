832da4f78db2fce7578228e4bb76b24b37e58d64
Davinderpal Singh <78408207+DavinderSingh810@users.noreply.github.com>
2024-08-06 00:53:16 +0530

    update

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -13,9 +13,10 @@ function Register() {
     }
   return (
     <>
-    <Formcontainer>
+    <FormContainer>
    <form onSubmit={(e)=>handleSubmit(e)}>
-    <div><img src={Logo} alt="Logo" />
+    <div className='brand'>
+      <img src={Logo} alt="Logo" />
     <h1>Chatup</h1>
     
     </div>
@@ -40,18 +41,91 @@ function Register() {
     onChange={(e)=>handleChange(e)}
     />
     
-   <button type='submt'> Create User</button>
+   <button type='submit'> Create User</button>
     <span>
         Already have an account? <Link to="/login">Login</Link>
     </span>
 
    </form>
     
-    </Formcontainer>
+    </FormContainer>
     </>
   )
 }
-const Formcontainer=styled.div``
+const FormContainer = styled.div`
+  
+  width: 100vw;
+  height:100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 5rem;
+    }
+    h1 {
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+    
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #00000076;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+     
+
+  }
+  input {
+    background-color: transparent;
+    padding: 1rem;
+    border: 0.1rem solid #4e0eff;
+    border-radius: 0.4rem;
+    color: white;
+    width: 100%;
+    font-size: 1rem;
+    &:focus {
+      border: 0.1rem solid #997af0;
+      outline: none;
+    }
+  }
+  button {
+    background-color: #997af0;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    &:hover {
+      background-color: #4e0eff;
+    }
+  }
+  span {
+    color: white;
+    text-transform: uppercase;
+    a {
+      color: #4e0eff;
+      text-decoration: none;
+      font-weight: bold;
+    }
+  }
+    
+`;
 
 
 export default Register
+
